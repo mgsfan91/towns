fbf6d60891476e9efb1dda43a448633d721d58f5
mgsfan91 <ivan.rubil@yahoo.com>
2019-10-29 04:32:08 +0100

    init 10 town INSERTs

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,9 +1,12 @@
 INSERT INTO towns (id, name, description, population, popularity_cnt)
 VALUES
 (1,'Dubrovnik','biser Jadrana', 40000, 0),
-(2,'Vukovar','na Dunavu', 30000, 5),
-(3,'Rijeka','najveca luka u RH', 120000, 3);
-
-
-INSERT INTO users (username, password, enabled)
-VALUES ('test','test',1);
+(2,'Vukovar','na Dunavu', 30000, 0),
+(3,'Rijeka','najveca luka u RH', 120000, 0),
+(4,'Zagreb','glavni grad', 700000, 0),
+(5,'Rijeka','najveca luka u RH', 120000, 0),
+(6,'Slavonski Brod','najveci u posavini', 60000, 0),
+(7,'Zadar','nalaziste poznatih morskih orgulja', 70000, 0),
+(8,'Osijek','najveci centar Slavonije', 120000, 0),
+(9,'Umag','najpoznatiji teniski turni', 30000, 0),
+(10,'Sisak','koncentracija teske industrije', 50000, 0);
