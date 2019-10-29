8ae8a3114e5577c03100b8e7d4dd2510166767b8
mgsfan91 <ivan.rubil@yahoo.com>
2019-10-29 08:22:42 +0100

    refactor security

--- src/main/java/com/example/towns/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/towns/configuration/WebSecurityConfig.java
@@ -9,56 +9,50 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Autowired
-    UserDetailsService userDetailsService;
+    UserDetailsService userDetailsServiceImpl;
 
     @Autowired
-    OncePerRequestFilter theFilter;
+    OncePerRequestFilter customTokenValidateFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsServiceImpl);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/authenticate").permitAll()
-                    .antMatchers("/h2-console/**").permitAll()
-                    .antMatchers("/towns/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .headers().frameOptions().disable()
+        .csrf().disable()
+        .authorizeRequests()
+            .antMatchers("/authenticate", "/register").permitAll()
+            .antMatchers("/towns/**").permitAll()
+        .anyRequest().authenticated()
         .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-                .addFilterBefore(theFilter, UsernamePasswordAuthenticationFilter.class)
-                ;
-
+        .addFilterBefore(customTokenValidateFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 
     @Bean
-    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
